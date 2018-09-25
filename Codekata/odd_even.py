a23c28ccb26b2550433a046e5c593041361965ef
irfanpeekay <42800787+irfanpeekay@users.noreply.github.com>
2018-09-25 09:23:50 +0530

    Update odd_even.py

--- Codekata/odd_even.py
+++ Codekata/odd_even.py
@@ -1,7 +1,11 @@
 a = int(input("Input:"))
-if a % 2 == 0:
-	print("\n Even")
-elif a % 2 != 0:
-	print("\n Odd")
+if a > 0:
+	if a % 2 == 0:
+		print("\nEven")
+	
+	elif a % 2 != 0:
+		print("\nOdd")
+	else:
+		print("\nInvalid")
 else:
-	print("\n Invalid")
+	print("\nInvalid")
